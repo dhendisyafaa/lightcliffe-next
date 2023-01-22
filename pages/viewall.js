0e6ba8956ed6fe008dd9196e21e6221ae260ddad
Dhendi Syafa <dhendisyafa@gmail.com>
2023-01-22 22:46:40 +0700

    feat: add responsive for card view all

--- pages/viewall.js
+++ pages/viewall.js
@@ -51,7 +51,7 @@ export default class viewall extends Component {
     return (
       <>
         <HeaderDetailComponent />
-        <div className='my-6 mx-3 border-4 grid grid-cols-2 justify-center items-center gap-3'>
+        <div className='my-6 mx-3 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-center items-center gap-x-1 gap-y-3'>
           {this.navLink.map(({ nameProduct, priceProduct, releaseDate, authorName }, index) =>
             <CardProduct nameProduct={nameProduct} priceProduct={priceProduct} releaseDate={releaseDate} authorName={authorName} key={index}/>
           )}
